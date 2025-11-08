416b1d21e5844019cade8bebf5df5d6b31938b4d
esra-ulubaba <esra.ulubaba1@gmail.com>
2025-11-08 17:09:46 +0300

    Update server.py for Railway deployment

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
+import eventlet
+import eventlet.wsgi
+import os
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
@@ -34,4 +37,6 @@ def disconnect():
     emit('updateLeaderboard', players, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    eventlet.wsgi.server(eventlet.listen(('', port)), app)
+
